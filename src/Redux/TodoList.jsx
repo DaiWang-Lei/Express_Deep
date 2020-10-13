6857b2161cca5a6614870d79b2d09da2e9acadc7
代罔 <1403693160@qq.com>
2020-10-13 23:24:02 +0800

    reduxInit done

--- src/Redux/TodoList.jsx
+++ src/Redux/TodoList.jsx
@@ -1,35 +1,50 @@
-import React, { Component } from 'react';
-import { Input, Button, List } from 'antd'
-import store from './store'
-import 'antd/dist/antd.css'
-
+import React, { Component } from "react";
+import { Input, Button, List } from "antd";
+import store from "./store";
+import "antd/dist/antd.css";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = store.getState();
-    }
-    state = {}
-    render() {
-        return (
-            <div style={{ margin: '10px' }}>
-                <div>
-                    <Input placeholder='React' style={{ width: '250px' }} />
-                    <Button type='primary'>增加</Button>
-                </div>
-                <div style={{margin:'10px',width:'300px'}}>
-                    <List
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={item => (<List.Item>{item}</List.Item>)}
-                    />
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.changeInputValue = this.changeInputValue.bind(this);
+
+    this.storeChange = this.storeChange.bind(this) //改变this指向
+    store.subscribe(this.storeChange)   //订阅Redux的状态
+  }
+  state = {};
+  render() {
+    return (
+      <div style={{ margin: "10px" }}>
+        <div>
+          <Input
+            placeholder="React"
+            style={{ width: "250px" }}
+            onChange={this.changeInputValue}
+          />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: "10px", width: "300px" }}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  changeInputValue(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value,
+    };
+    store.dispatch(action);
+  }
+  storeChange(){
+      this.setState(store.getState())
+  }
 }
-
-
-
 
 export default TodoList;
--- src/Redux/store/reducer.js
+++ src/Redux/store/reducer.js
@@ -1,14 +1,18 @@
 //默认数据
 const defaultState = {
-    inputValue:'React ahhhhh',
-    list:[
-        '第一周，深入理解React',
-        '第二周，深入理解Redux'
-    ]
-}
-
+  inputValue: "React ahhhhh",
+  list: ["第一周，深入理解React", "第二周，深入理解Redux"],
+};
 
 // state：整个项目中需要管理的数据信息，这里没有数据，所以用空对象表示
-export default (state = defaultState, action) => { //方法函数
-    return state
-}
+export default (state = defaultState, action) => {
+  //方法函数
+  switch (action.type) {
+    case "change_input_value":
+      let newState = JSON.parse(JSON.stringify(state));
+      newState.inputValue = action.value;
+      return newState;
+    default:
+      return state;
+  }
+};
